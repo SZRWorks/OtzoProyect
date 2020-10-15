bd2237339f6bb5e80764ec62e2d8db8946153817
Gael <Gael@192.168.1.64>
2020-10-15 09:18:10 -0500

    Conexion PDO y config

---
+++ Scripts/php/Conexion.inc.php
@@ -0,0 +1,30 @@
+<?php
+class Conexion{
+    
+    private static $conexion;
+    
+    public static function abrirConexion(){
+        if(!isset(self::$conexion)){
+            try{
+                include_once 'config.inc.php';
+                self::$conexion = new PDO('mysql:host='.NOMBRE_SERVIDOR. '; dbname='.NOMBRE_BD, NOMBRE_USUARIO, PASSWORD);
+                self::$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conexion -> exec("SET CHARACTER SET utf8");
+            } catch (PDOException $ex) {
+                print "ERROR: ". $ex ->getMessage() . "<br>";
+                die();
+            }
+        }
+    }
+    
+    public static function cerrarConexion(){
+        if(isset(self::$conexion)){
+            self::$conexion = null;
+        }
+    }
+    
+    public static function getConexion() {
+        return self::$conexion;
+    }
+  
+}
---
+++ Scripts/php/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+//Base de datos
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'root');
+define('PASSWORD', '');
+define('NOMBRE_BD', 'prototipos');
+
+//URLS
+
+define('SERVIDOR', 'http://localhost/PlugGet');
+
+/*
+define('RUTA_REGISTRO', SERVIDOR."/Registro.php");
+define('RUTA_REGISTRO_CORRECTO',SERVIDOR."/registroCorrecto.php");
+define('RUTA_LOGIN', SERVIDOR."/login.php");
+define('RUTA_LOGOUT', SERVIDOR."/logout.php");
+  */
+ 
