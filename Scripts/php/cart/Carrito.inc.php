c9266208d2b6c15eefe9b80609ac603efacaaa5e
TheSZR <saulzepedarob@gmail.com>
2020-10-23 10:01:54 -0500

    ...

--- Scripts/php/cart/Carrito.inc.php
+++ Scripts/php/cart/Carrito.inc.php
@@ -105,12 +105,7 @@ class Carrito {
         . '<td>' . $data['Titulo'] . '</td>'
         . '<td>$' . $data['Precio'] . '</td>'
         . '<td><form><input type="number" value="' . $cantidad . '" name="cantidad"><input type="hidden" name="id" value="'.$id.'" /></form></td>'
-<<<<<<< HEAD
         . '<td><a class="btn text-danger" href="styled_cart.php?eliminar='.$id.'" role="button">Eliminar</a></td>'
-=======
-        . '<td> $'.($this->subtotal[] = $data['Precio'] * $cantidad).'</td>'
-        . '<td><a class="btn btn-primary" href="styled_cart.php?eliminar='.$id.'" role="button">Eliminar</a></td>'
->>>>>>> 04e606780746b551109feb0692e760d31373297d
         . '</tr>';
     }
 
