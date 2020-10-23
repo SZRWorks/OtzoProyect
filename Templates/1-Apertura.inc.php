1b1d3b6425e8f2be44070260a02f816fc103cb03
TheSZR <saulzepedarob@gmail.com>
2020-10-23 11:18:22 -0500

    Favicon Added

--- Templates/1-Apertura.inc.php
+++ Templates/1-Apertura.inc.php
@@ -12,6 +12,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="icon" href="Imagenes/favicon.png" type="image/png" />
     
     <!-- Agregar titulo a la pestaña -->
     <?php
